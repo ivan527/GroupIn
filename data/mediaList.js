dcdc7042e4b6d325b1dcc73c7fdf164542031aa9
Ivan Ching <iching@stevens.edu>
2017-05-11 15:48:05 -0400

    Add api calls for adding members and media to media list

--- data/mediaList.js
+++ data/mediaList.js
@@ -40,9 +40,70 @@ let exportedMethods = {
 		return mediaList().then((mediaListCollection) => {
 			return mediaListCollection.removeOne({ _id: id }).then((deletionInfo) => {
 				if (deletionInfo.deletedCount === 0) {
-					return Promise.reject('Could not delete ')
+					return Promise.reject(`Could not delete media list with id of ${id}`)
 				}
-			})
-		})
+			});
+		});
+	},
+
+	addMember(mediaListId, memberId) {
+		return this.getMediaListById(mediaListId).then((currentMediaList) => {
+			let newMemberListInfo = {
+				members: currentMediaList.members.append(memberId)
+			};
+
+			let updateCommand = {
+				$set: newMemberListInfo
+			};
+
+			return mediaList().then((mediaListCollection) => {
+				return mediaListCollection.updateOne({ _id: mediaListId }, updateCommand).then(() => {
+					return this.getMediaListById(mediaListId);
+				});
+			});
+		});
+	},
+
+	addMedia(mediaListId, media) {
+		return this.getMediaListById(mediaListId).then((currentMediaList) => {
+			let newMemberListInfo = {
+				media: currentMediaList.media.append(media)
+			};
+
+			let updateCommand = {
+				$set: newMemberListInfo
+			};
+
+			return mediaList().then((mediaListCollection) => {
+				return mediaListCollection.updateOne({ _id: mediaListId }, updateCommand).then(() => {
+					return this.getMediaListById(mediaListId);
+				});
+			});
+		});
+	},
+
+	removeMediaByTitle(mediaListId, mediaTitle) {
+		return this.getMediaListById(mediaListId).then((currentMediaList) => {
+			let i;
+			for (i = 0; i < currentMediaList.media.length; i++) {
+				if (currentMediaList.media[i].Title == mediaTitle) {
+					break;
+				}
+			}
+
+			let newMediaListInfo = {
+				media: currentMediaList.media.splice(i, 1);
+			};
+
+			let updateCommand = {
+				$set: newMediaListInfo
+			};
+
+			return mediaList().then((mediaListCollection) => {
+				return mediaListCollection.updateOne({ _id: mediaListId }, updateCommand).then(() => {
+					return this.getMediaListById(mediaListId)
+				});
+			});
+		});
 	}
 }
