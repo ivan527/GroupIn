cf3563acd1be92b16386ef74d9dcd1153c3eea20
Jake Israel <jcmisrael@gmail.com>
2017-05-12 18:07:40 -0400

    Adds user routes for adding a user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 // routes/index.js
 const authCheck = require("./authenticate.js");
+const userRoutes = require("./user.js");
 
 const constructorMethod = (app) => {
     app.use("/", authCheck);
+    app.use("/user", userRoutes);
 
     app.use("*", (req, res) => {
         res.status(404).json({error: "Page not found"});
