b0dd439d1fd558594582ad04902a310b835d10c7
Anthony Jin <ajin@stevens.edu>
2017-05-13 17:17:15 -0400

    handlebars

--- routes/mediaList.js
+++ routes/mediaList.js
@@ -15,7 +15,25 @@ router.post("/addMember", (req, res) => {
 });
 
 router.post("/addMedia", (req, res) => {
+	let formInfo = req.body;
+	let errors = []
+	if (!formInfo.mediaTitle){
+		errors.push("No title provided");
+	}
 
+	if(!formInfo.mediaType){
+		errors.push("No type provided");
+	}
+
+	if(!formInfo.numEpisodes){
+		errors.push("No number of Episodes provided");
+	}
+	
+	if(!formInfo.refLink){
+		errors.push("No reference link provided");
+	}
+
+	
 });
 
 router.post("/:Title", (req, res) => {
