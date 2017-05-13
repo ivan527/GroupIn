080581c2da85a51a4709c04e0367ea41a3f83cc2
Jake Israel <jcmisrael@gmail.com>
2017-05-13 17:22:36 -0400

    Updates login-signup form and enables signup

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const router = express.Router();
 const data = require("../data");
 const userData = data.users;
 const mediaListData = data.mediaList;
 
-router.post("/add", (req, res) => {
-    let userArgs = req.body;
-    userData.addUser(userArgs.username, userArgs.password).then((user) => {
-        res.json(user);
-    }, () => {
-        res.sendStatus(500);
-    });
-});
-
-router.get("/hub", (req, res) => {
+const router = express.Router();
+router.get("/profile", (req, res) => {
     let profile = req.user.profile;
     let mediaListArray =
     mediaListData.getMediaListsByUser(req.user._id).then((mediaLists) => {
